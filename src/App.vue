34e39a65a7832f3c95a834cb3cbeeb8e0b8f021e
PBJimm <20ftt1813@student.pb.edu.bn>
2022-04-14 10:51:28 +0800

    added the build.css

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,21 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img class="w-35 h-48 mx-auto" src="./assets/lemon.jpg" alt="">
+  <!-- <img class="w-35 h-48 mx-auto" src="./assets/lemon.jpg" alt="">
   <p>ROHZEIMEE BIN NENDAROH</p>
   <p>STUDENT OF POLITEKNIK BRUNEI</p>
   <p>WEB DEVELOPMENT DWDT06</p>
   <p>20FTT1813</p>
-  <p>20FTT1813@STUDENT.PB.EDU.BN</p>
+  <p>20FTT1813@STUDENT.PB.EDU.BN</p> -->
+  <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+  <div class="shrink-0">
+    <img class="h-30 w-48" src="./assets/lemon.jpg" alt="ChitChat Logo">
+  </div>
+  <div>
+    <div class="text-xl font-medium text-black">Rohzeimee Bin Nendaroh</div>
+    <p class="text-slate-500">Student of Politeknik Brunei</p>
+  </div>
+</div>
 </template>
 
 <style>
